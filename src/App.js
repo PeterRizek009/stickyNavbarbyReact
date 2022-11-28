d501d2ccdfa25f459c1a84d3e29e26fd2c1160ac
Peter Shahin <petershahin00@gmail.com>
2022-11-28 18:28:01 +0200

    edit nav

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import Navbar from './stickyNavbar/navbar';
+
+function App() {
+  return (
+    <div className="App">
+       <Navbar/>
+    </div>
+  );
+}
+
+export default App;
